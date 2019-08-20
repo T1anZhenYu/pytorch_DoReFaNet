2fd27ff0e59bbb09fd5b789a0828375db79f0fc7
tian <t18215595124@gmail.com>
2019-08-20 14:35:34 +0800

    Update dataloader.py

--- dataloader.py
+++ dataloader.py
@@ -2,16 +2,18 @@ import torch
 from torch.utils.data.dataset import Dataset
 import os 
 import numpy as np 
+from sklearn.model_selection import train_test_split
 class SmartWall(Dataset):
-    def __init__(self, data_path):
+    def __init__(self, data,label):
         """
         Args:
             csv_path (string): path to csv file
             img_path (string): path to the folder where images are
             transform: pytorch transforms for transforms and tensor conversion
         """
-
-        self.data_raw = dict(np.load(data_path,allow_pickle=True))['arr_0']
+        self.data = data
+        self.label = label
+        #self.data_raw = dict(np.load(data_path,allow_pickle=True))['arr_0']
         '''
         print(data_raw.shape)
         self.data=  np.expand_dims(data_raw[0]['data'],axis=0)
@@ -26,10 +28,11 @@ class SmartWall(Dataset):
                 print('read_data\n',data_raw[j]['data'])               
         self.data = self.data
         '''
-        self.data_len=len(self.data_raw)
+        #self.data_len=len(self.data_raw)
 
     def __getitem__(self, index):
-
+        return torch.unsqueeze(torch.unsqueeze(self.data[index,:,:],0),0),self.label[index]
+        '''
         temp = torch.unsqueeze(torch.cuda.FloatTensor(self.data_raw[index]['data']),-1)
         if temp.shape[0]==0:
             index -= 1
@@ -42,13 +45,15 @@ class SmartWall(Dataset):
             single_label = self.data_raw[index]['label']           
 
         return (torch.transpose(single_data.repeat(4,1,1),0,2), single_label)
-
+        '''
     def __len__(self):
-        return self.data_len
+        return self.data.shape[0]
 
 def data_loader(train_batch_size,test_batch_size):
-    train_root='/content/drive/My Drive/smart_wall_data/train.npz'
-    test_root='/content/drive/My Drive/smart_wall_data/test.npz'
+    x = np.load('/content/drive/My Drive/smart_wall_data/X.npy')
+    y = np.load('/content/drive/My Drive/smart_wall_data/y.npy')
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=0,stratify=y)
+
     train_dataset=SmartWall(train_root)
     test_dataset=SmartWall(test_root)
     train_loader=torch.utils.data.DataLoader(
@@ -60,4 +65,5 @@ def data_loader(train_batch_size,test_batch_size):
         batch_size=test_batch_size,
         shuffle=True
     )
+
     return train_loader,test_loader,len(test_dataset)
